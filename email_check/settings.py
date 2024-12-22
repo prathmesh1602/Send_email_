e146969a74cd62db61d398778eea7f9617a0b490
prathmesh1602 <131581984+prathmesh1602@users.noreply.github.com>
2024-12-22 20:53:25 +0530

    Update settings.py

--- email_check/settings.py
+++ email_check/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'munnashinde4889@gmail.com'
-EMAIL_HOST_PASSWORD = 'jkvs pkbb txaa faeb'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
